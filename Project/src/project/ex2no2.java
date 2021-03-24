dbf27862d6278898e27de43c0ea9afe7ed6559ac
admin <admin@DESKTOP-8L2IG92>
2021-03-24 21:22:59 +0900

    에제 2-2문제

---
+++ Project/src/project/ex2no2.java
@@ -0,0 +1,21 @@
+package project;
+
+public class ex2no2 {
+     public static void main(String[] args) {
+	  int x =10,y=20;
+	  int tmp = 0;
+	  
+	  System.out.println("X: "+x+"Y: "+y);
+	  
+	  tmp =x;
+	  x=y;
+	  y=tmp;
+	  System.out.println("X: "+x+"Y: "+y);
+    	 
+    	 
+    	 
+}
+	
+	
+	
+}
